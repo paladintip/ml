2e1ab5c06196eaa1191fe80ed1d22cb9a973ccb1
Paladin/Masayume <ritefact@yahoo.com>
2019-09-24 15:58:45 -0700

    Update ml-predict.php

--- api/ml-predict.php
+++ api/ml-predict.php
@@ -2,12 +2,15 @@
 
 $command = escapeshellcmd('cd /var/www/optimal-work-hours/ &&
 python3 execute-pickle-model.py 4');
-$output = shell_exec ( $command );
-var_dump($output);
-$data['message'] = $output;
+$last_line = system($command, $retval);
 
+var_dump($last_line);
+$data['message'] = $last_line;
+
+echo '
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
 
 header('Content-Type: application/json');
 echo json_encode($data);
 
-#!/usr/bin/env python
